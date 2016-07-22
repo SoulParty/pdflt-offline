0e81e20188dfb5d26c40d1c57b88b7960c0e1082
DK <danielius.kibartas@nortal.com>
2016-07-22 10:50:42 +0300

    test

--- src/test/java/lt/nortal/pdflt/TestReadSmartCard.java
+++ src/test/java/lt/nortal/pdflt/TestReadSmartCard.java
@@ -2,19 +2,9 @@ package lt.nortal.pdflt;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.math.BigInteger;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.PrivateKey;
-import java.security.Provider;
-import java.security.Security;
-import java.security.Signature;
-import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.security.interfaces.RSAPublicKey;
 import java.util.Properties;
-import java.util.logging.Logger;
 
 import org.junit.Test;
 
@@ -30,12 +20,6 @@ import iaik.pkcs.pkcs11.objects.X509PublicKeyCertificate;
 import iaik.pkcs.pkcs11.provider.Constants;
 import iaik.pkcs.pkcs11.provider.IAIKPkcs11;
 import iaik.pkcs.pkcs11.wrapper.PKCS11Constants;
-import lt.nortal.components.unisign.applet.infrastructure.InfrastructureException;
-import sun.nio.cs.StandardCharsets;
-import sun.security.pkcs11.SunPKCS11;
-
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.Hex;
 
 /**
  * Created by DK on 7/19/16.
@@ -55,73 +39,8 @@ public class TestReadSmartCard {
 			// list all slots (readers) in which there is currenlty a token present
 			Slot[] slotsWithToken =
 					module.getSlotList(Module.SlotRequirement.TOKEN_PRESENT);
-//			Token token = slotsWithToken[0].getToken();
-			Token token = slotsWithToken[1].getToken();
-			Session session =
-					token.openSession(Token.SessionType.SERIAL_SESSION,
-							Token.SessionReadWriteBehavior.RO_SESSION,
-							null,
-							null);
-
-			session.login(Session.UserType.USER, "04033".toCharArray());
-
-			// we search for a RSA private key which we can use for signing
-			RSAPrivateKey searchTemplate = new RSAPrivateKey();
-			searchTemplate.getSign().setBooleanValue(Boolean.TRUE);
-			// search for a key
-			session.findObjectsInit(searchTemplate);
-			Object[] matchingKeys = session.findObjects(1);
-			RSAPrivateKey signatureKey = null;
-			if (matchingKeys.length > 0) {
-				session.findObjectsFinal();
-				signatureKey = (RSAPrivateKey) matchingKeys[0];
-				byte[] data = "hello world".getBytes();
-				// select the signature mechanism, ensure your token supports it
-				Mechanism signatureMechanism = Mechanism.get(PKCS11Constants.CKM_RSA_PKCS);
-				// initialize for signing
-				session.signInit(signatureMechanism, signatureKey);
-				byte[] signatureValue = session.sign(data);
-				System.out.println(signatureValue);
-
-				byte[] keyID = signatureKey.getId().getByteArrayValue();
-				// this is the implementation that uses a concrete object class (X509PublicKeyCertificate) for
-				// searching
-				X509PublicKeyCertificate certificateSearchTemplate = new X509PublicKeyCertificate();
-				certificateSearchTemplate.getId().setByteArrayValue(keyID);
-				session.findObjectsInit(certificateSearchTemplate);
-				Object[] foundCertificateObjects = session.findObjects(1);
-				session.findObjectsFinal();
-				X509PublicKeyCertificate cert = (X509PublicKeyCertificate) foundCertificateObjects[0];
-				String pem = MacPKCS11Infrastructure.convertToPem(cert.getValue().toString());
-				InputStream stream = new ByteArrayInputStream(pem.getBytes("UTF-8"));
-				CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-				X509Certificate x509Certificate = (X509Certificate)certificateFactory.generateCertificate(stream);
-			} else {
-				session.findObjectsFinal();
-				// we have not found a suitable key, we cannot contiue
-			}
-			// do not forget to finish the find operation
-		} catch (Throwable ex) {
-			ex.printStackTrace();
-		}
-	}
-
-
-	@Test
-	public void testIAIK2() {
-		try {
-			Properties properties = new Properties();
-			properties.put(Constants.PKCS11_NATIVE_MODULE, "/Library/EstonianIDCard/lib/esteid-pkcs11.so");
-			properties.put(Constants.PKCS11_WRAPPER_PATH, "/Library/libpkcs11wrapper.jnilib");
-			Module module = IAIKPkcs11.getModule(properties);
-			module.initialize(new DefaultInitializeArgs());
-			Info info = module.getInfo();
-			System.out.println(info);
-			// list all slots (readers) in which there is currenlty a token present
-			Slot[] slotsWithToken =
-					module.getSlotList(Module.SlotRequirement.TOKEN_PRESENT);
-//			Token token = slotsWithToken[0].getToken();
-			Token token = slotsWithToken[1].getToken();
+//			Token token = slotsWithToken[0].getToken(); // Authentication token
+			Token token = slotsWithToken[1].getToken(); // Signature token
 			Session session =
 					token.openSession(Token.SessionType.SERIAL_SESSION,
 							Token.SessionReadWriteBehavior.RO_SESSION,
@@ -153,19 +72,16 @@ public class TestReadSmartCard {
 				String pem = MacPKCS11Infrastructure.convertToPem(cert.getValue().toString());
 				InputStream stream = new ByteArrayInputStream(pem.getBytes("UTF-8"));
 				CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-				X509Certificate x509Certificate = (X509Certificate)certificateFactory.generateCertificate(stream);
-
-
-//128 baitai
+				X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(stream);
 
 				byte[] data =
 //						("Hello-World!!!!!!!!aa"
 //								+ "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-//								+ "!!!!!!!!!!!!!!!!!!!!!!")
-				("Hello-World!!!!!!!!aa"
+//								+ "!!!!!!!!!!!!!!!!!!!!!!") // < 128 baitai
+						("Hello-World!!!!!!!!aa"
 								+ "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-								+ "!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-						.getBytes();
+								+ "!!!!!!!!!!!!!!!!!!!!!!!!!!!") //128 baitai
+								.getBytes();
 //				byte[] data = "hello world".getBytes();
 				// select the signature mechanism, ensure your token supports it
 				Mechanism signatureMechanism = Mechanism.get(PKCS11Constants.CKM_RSA_PKCS);
@@ -174,7 +90,6 @@ public class TestReadSmartCard {
 				session.signInit(signatureMechanism, signatureKey);
 				byte[] signatureValue = session.sign(data);
 				System.out.println(signatureValue);
-
 
 			} else {
 				session.findObjectsFinal();
